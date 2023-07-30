23b3f59007112cab7815af51010680b009ba478d
Ramon Valerio da  Silva <ramon.dasilva@southworks.com>
2023-07-30 20:46:23 -0300

    add core project with the model

---
+++ SierraTakeHome.Core/Domain/Orders/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace SierraTakeHome.Core.Domain.Orders
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public required string Price { get; set; }
+    }
+}
---
+++ SierraTakeHome.Core/Domain/Products/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace SierraTakeHome.Core.Domain.Products
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public int CustomerID { get; set; }
+
+        public int ProductId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int TotalCost { get; set; }
+    }
+}
