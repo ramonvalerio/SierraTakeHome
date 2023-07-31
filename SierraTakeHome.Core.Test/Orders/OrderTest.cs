c9aa2c26b0f356c2cea7ae7e68377073adb979b9
Ramon Valerio da  Silva <ramon.dasilva@southworks.com>
2023-07-31 15:22:59 -0300

    add test layer

---
+++ SierraTakeHome.Core.Test/Orders/OrderTest.cs
@@ -0,0 +1,17 @@
+﻿namespace SierraTakeHome.Core.Test.Orders
+{
+    public class OrderTest
+    {
+        [Fact]
+        public void Create_Order()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Validate_Order()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ SierraTakeHome.Core.Test/Products/ProductTest.cs
@@ -0,0 +1,17 @@
+namespace SierraTakeHome.Core.Test.Products
+{
+    public class ProductTest
+    {
+        [Fact]
+        public void Create_Product()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Validate_Product()
+        {
+            Assert.True(true);
+        }
+    }
+}
