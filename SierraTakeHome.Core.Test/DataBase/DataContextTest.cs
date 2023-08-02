785698be6acdefb9af71944903a13c761656a449
Ramon Valerio da  Silva <ramon.dasilva@southworks.com>
2023-08-02 11:40:32 -0300

    add test for datacontext

---
+++ SierraTakeHome.Core.Test/DataBase/DataContextTest.cs
@@ -0,0 +1,17 @@
+﻿using SierraTakeHome.Core.Data;
+
+namespace SierraTakeHome.Core.Test.DataBase
+{
+    public class DataContextTest
+    {
+        [Fact]
+        public void CreateDataContextWithConnectionStringFromAppSettings()
+        {
+            var dataContextFactory = new DesignTimeDbContextFactory();
+
+            var dbContext = dataContextFactory.CreateDbContext(null);
+
+            
+        }
+    }
+}
