ac8bfdc18ffc2b4a2e5719612fc23d066fc89ecb
Ramon Valerio da  Silva <ramon.dasilva@southworks.com>
2023-08-02 11:27:34 -0300

    fix script now it is using migrations to create the tables

--- scripts/init.sql
+++ scripts/init.sql
@@ -4,42 +4,6 @@ GO
 USE SierraTakeHome_DB
 GO
 
--- Create Tables
-CREATE TABLE TB_PRODUCT (
-    Id INT PRIMARY KEY IDENTITY(1,1),
-    Name NVARCHAR(255) NOT NULL,
-    Price DECIMAL(18, 2) NOT NULL
-)
-GO
-
-CREATE TABLE TB_CUSTOMER (
-    Id INT PRIMARY KEY IDENTITY(1,1),
-    Name NVARCHAR(255) NOT NULL
-)
-GO
-
-CREATE TABLE TB_ORDER (
-    Id INT PRIMARY KEY IDENTITY(1,1),
-    CustomerID INT NOT NULL,
-    ProductId INT NOT NULL,
-    Quantity INT NOT NULL,
-    TotalCost INT NOT NULL,
-    FOREIGN KEY (ProductId) REFERENCES TB_PRODUCT(Id)
-)
-GO
-
--- Insert data
-INSERT INTO TB_PRODUCT (Name, Price) VALUES ('Product 1', 10.00);
-INSERT INTO TB_PRODUCT (Name, Price) VALUES ('Product 2', 20.25);
-INSERT INTO TB_PRODUCT (Name, Price) VALUES ('Product 3', 30.50);
-INSERT INTO TB_PRODUCT (Name, Price) VALUES ('Product 4', 50.75);
-INSERT INTO TB_PRODUCT (Name, Price) VALUES ('Product 5', 100.00);
-
-INSERT INTO TB_CUSTOMER (Name) VALUES ('Ramon Valerio');
-INSERT INTO TB_CUSTOMER (Name) VALUES ('Thor');
-INSERT INTO TB_CUSTOMER (Name) VALUES ('Batman');
-GO
-
 -- Create Procedure
 CREATE PROCEDURE CreateOrder
     @CustomerID INT,
@@ -47,8 +11,8 @@ CREATE PROCEDURE CreateOrder
     @Quantity INT
 AS
 BEGIN
-    DECLARE @Price DECIMAL(18, 2)
-    DECLARE @TotalCost DECIMAL(18, 2)
+    DECLARE @Price FLOAT
+    DECLARE @TotalCost FLOAT
 
     -- Get product price
     SELECT @Price = Price FROM TB_PRODUCT WHERE Id = @ProductID
