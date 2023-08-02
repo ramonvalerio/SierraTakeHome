0e4f782f9be0b9ead587ff493ccc2bc2386b9c36
Ramon Valerio da  Silva <ramon.dasilva@southworks.com>
2023-08-02 17:05:48 -0300

    add login component

---
+++ sierratakehome-ui/src/components/Login/Login.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import './Login.css';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Do the logic of Login here
+  };
+
+  return (
+    <div className="login-container">
+      <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+      <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default Login;
